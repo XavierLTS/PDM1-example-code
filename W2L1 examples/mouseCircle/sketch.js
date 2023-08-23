9da7fd7cdc6c36baad37274a1e3e6f4a5d67b95f
ace01603 <50892754+ace01603@users.noreply.github.com>
2023-08-23 07:31:45 +0100

    W2L1 examples added

---
+++ W2L1 examples/mouseCircle/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 300);
+}
+
+function draw() {
+    let x = 0;
+    background(0);
+    // mouseX and mouseY are variables that store the current coordinates of the mouse
+    circle(mouseX, mouseY, 40);
+}
