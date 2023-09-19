305217aa4e728c7e410307bd879ccdd67490a475
ace01603 <50892754+ace01603@users.noreply.github.com>
2023-09-19 14:26:07 +0100

    fixed organisation hiccup

--- Lectures/W1L2 examples/ellipse/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    createCanvas(400, 300);
-}
-
-function draw() {
-    background(150);
-    //ellipse(200, 150, 300, 200);
-    circle(200, 150, 300);
-}
--- Lectures/W1L2 examples/pinkDot/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    createCanvas(600, 400);
-}
-
-function draw() {
-    background(255);
-    fill(237, 55, 222);
-    circle(150, 100, 20);
-}
